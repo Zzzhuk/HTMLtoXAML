649e9b0672bd5f718b5e847a4a3e4040fe8c809e
Alexey Zhuk <zhuk.alexey@tabiallc.com>
2019-05-31 18:33:46 +0200

    added FileWatcher

--- ViewModel.cs
+++ ViewModel.cs
@@ -19,9 +19,30 @@ namespace HTMLtoXAML
 		public Model model = new Model();
 		public FileWatcher fw = new FileWatcher();
 		public string filePath;
-
-		private DefaultDialogService dialogService = new DefaultDialogService();
 		
+		private DefaultDialogService dialogService = new DefaultDialogService();
+
+		private string bgcolor = "#FF00AB5E";
+		public string bgColor
+		{
+			get { return bgcolor; }
+			set
+			{
+				bgcolor = value;
+				OnPropertyChanged("bgColor");
+			}
+		}
+		private string btntext = "Start";
+		public string btnText
+		{
+			get { return btntext; }
+			set
+			{
+				btntext = value;
+				OnPropertyChanged("btnText");
+			}
+		}
+
 		private RelayCommand openFile;
 		public RelayCommand OpenFile
 		{
@@ -44,23 +65,35 @@ namespace HTMLtoXAML
 				  }));
 			}
 		}
-		public async Task Foo(string path)
+		public FileSystemWatcher watcher;
+		private void funcChangedHandler(object sender, FileSystemEventArgs e)
 		{
-			await fw.WhenFileUpdate(path);
-			MessageBox.Show("Eeeboy");
+			if (e.Name == Path.GetFileName(filePath))
+			{
+				MessageBox.Show("File updated");
+			}
 		}
+
+
 		private RelayCommand startWatcher;
 		public RelayCommand StartWatcher
 		{
 			get
 			{
 				return startWatcher ??
-				  (startWatcher = new RelayCommand(async obj =>
+				  (startWatcher = new RelayCommand(obj =>
 				  {
+					  if (watcher != null)
+						  return;
+					  bgColor = "#FFAB0000";
+					  btnText = "Stop";
 					  try
 					  {
-						  await Foo(filePath);
-;					  }
+						  watcher = new FileSystemWatcher(Path.GetDirectoryName(filePath));
+
+						  watcher.Changed += funcChangedHandler;
+						  watcher.EnableRaisingEvents = true; 
+						}
 					  catch (Exception ex)
 					  {
 						  dialogService.ShowMessage(ex.Message);
@@ -76,9 +109,15 @@ namespace HTMLtoXAML
 				return stopWatcher ??
 				  (stopWatcher = new RelayCommand(obj =>
 				  {
+					  if(watcher == null)
+						  return;
+					  bgColor = "#FF00AB5E";
+					  btnText = "Start";
 					  try
 					  {
-						  
+						  watcher.Changed -= funcChangedHandler;
+						  watcher.Dispose();
+						  watcher = null;
 					  }
 					  catch (Exception ex)
 					  {
